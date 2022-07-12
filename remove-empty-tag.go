e0cf5a731a920c419116cde0c5faf53aa3306d15
earlgray <earlgray283@gmail.com>
2022-07-12 15:32:55 +0900

    feat: csv 加工ツール

--- remove-empty-tag.go
+++ remove-empty-tag.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
+	"log"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Tag struct {
@@ -16,10 +19,11 @@ type Tag struct {
 
 type Geotag struct {
 	id        uint64
-	date      string
+	elapsed   uint64
 	latitude  float64
 	longitude float64
-	url       string
+	farmNum   int    // http://farm{farmNum}.flickr...
+	directory string // /8237/8520927781_4f86a7a3b1.jpg
 }
 
 func main() {
@@ -38,6 +42,14 @@ func main() {
 	sort.Slice(tags, func(i, j int) bool {
 		return tags[i].id < tags[j].id
 	})
+	newTagFile, _ := os.Create("csv/new_tag.csv")
+	newTagCsv := csv.NewWriter(newTagFile)
+	for _, tag := range tags {
+		cols := []string{strconv.FormatUint(tag.id, 10), tag.tag}
+		newTagCsv.Write(cols)
+	}
+	newTagCsv.Flush()
+	newTagFile.Close()
 
 	geotags := make([]*Geotag, 0)
 	geotagFile, _ := os.Open("csv/geotag.csv")
@@ -45,23 +57,44 @@ func main() {
 	for geotagsc.Scan() {
 		tokens := strings.Split(strings.TrimSpace(geotagsc.Text()), ",")
 		id, _ := strconv.ParseUint(tokens[0], 10, 64)
+		baseDate := time.Date(2012, time.January, 1, 0, 0, 0, 0, time.Local)
+		date, err := time.Parse("2006-01-02 15:04:05", strings.Trim(tokens[1], "\""))
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		latitude, _ := strconv.ParseFloat(tokens[2], 64)
 		longitude, _ := strconv.ParseFloat(tokens[3], 64)
+		var farmNum int
+		var directory string
+		fmt.Sscanf(tokens[4], "http://farm%d.static.flickr.com%s", &farmNum, &directory)
 		geotags = append(geotags, &Geotag{
 			id:        id,
-			date:      tokens[1],
+			elapsed:   uint64(date.Sub(baseDate).Seconds()),
 			latitude:  latitude,
 			longitude: longitude,
-			url:       tokens[4],
+			farmNum:   farmNum,
+			directory: directory,
 		})
 	}
 	geotagFile.Close()
+	sort.Slice(geotags, func(i, j int) bool {
+		return geotags[i].id < tags[j].id
+	})
 
-	cnt := 0
-	for _, tag := range tags {
-		if tag.tag == "" {
-			cnt++
+	newGeotagFile, _ := os.Create("csv/new_geotag.csv")
+	newGeotagCsv := csv.NewWriter(newGeotagFile)
+	for _, geotag := range geotags {
+		cols := []string{
+			strconv.FormatUint(geotag.id, 10),
+			strconv.FormatUint(geotag.elapsed, 10),
+			strconv.FormatFloat(geotag.latitude, 'f', -1, 64),
+			strconv.FormatFloat(geotag.longitude, 'f', -1, 64),
+			strconv.FormatUint(uint64(geotag.farmNum), 10),
+			geotag.directory,
 		}
+		newGeotagCsv.Write(cols)
 	}
-	fmt.Println(cnt)
+	newGeotagCsv.Flush()
+	newGeotagFile.Close()
 }
